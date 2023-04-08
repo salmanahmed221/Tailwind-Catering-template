eab0dd4d48895cfffe17454e2478f28c275cabb4
unknown <salmanahmed121221@gmail.com>
2023-04-08 19:04:24 +0500

    navbar working for mobile view

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,15 @@
+"use client"
 import Link from "next/link";
+import { useState } from "react";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { ImCross } from 'react-icons/im';
 
-export default function Navbar() {
-    return <div className="flex md:justify-between justify-center px-16 py-4 shadow-xl bg-white sticky top-0 z-10">
 
-        <div >
+function Navbar() {
+    const [nav, setNav] = useState(true)
+    return <div className="flex justify-between  md:px-16 px-4 items-center py-4 shadow-xl bg-white sticky top-0 z-10">
+
+        <div className="z-10">
             <h1 className="hover:bg-gray-200 font-bold md:font-normal font-mono">Gourmet Catering</h1>
         </div>
 
@@ -12,5 +18,19 @@ export default function Navbar() {
             <Link href={"#aboutpage"} target="_parent" className="hover:bg-gray-200">About</Link>
             <Link href={"#menupage"} target="_parent" className="hover:bg-gray-200">Menu</Link>
         </div>
-    </div>;
+
+        {/* Hamburger button */}
+        <div className="md:hidden text-xl z-10" onClick={() => { setNav(!nav) }}>
+            {nav ? <GiHamburgerMenu /> : <ImCross />}
+        </div>
+
+        {/* overlay */}
+        {!nav ? <div className="fixed top-0 left-0 flex items-center md:hidden justify-center flex-col w-full bg-white h-[50%] text-xl leading-10 font-mono">
+            <Link href={"#homepage"} target="_parent" className="hover:bg-gray-200">Home</Link>
+            <Link href={"#aboutpage"} target="_parent" className="hover:bg-gray-200">About</Link>
+            <Link href={"#menupage"} target="_parent" className="hover:bg-gray-200">Menu</Link>
+        </div> : null}
+    </div>
 }
+
+export default Navbar;
