a1bb7bb9c5261be66e862ecc1883cce391a290e2
unknown <salmanahmed121221@gmail.com>
2023-04-09 21:29:19 +0500

    Fix herosection image width

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function HeroSection() {
     return <div id="homepage">
-        <div className="">
-            <Image src={"/hamburger2.jpg"} alt="" width={1300} height={100} />
+        <div >
+            <Image src={"/hamburger2.jpg"} alt="" width={1300} height={100} className="w-screen" />
         </div>
     </div>;
 }
